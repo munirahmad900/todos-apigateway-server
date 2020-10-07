9cac16207621128f8efc6806ed90654ad14d8dd1
Munir <munir.ahmad900@gmail.com>
2020-10-07 23:42:07 +0500

    first commit

---
+++ src/test/java/com/todos/apigateway/service/todosapigatewayserver/TodosApigatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todos.apigateway.service.todosapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApigatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
